e23e4e11397ae4687ddf6257c6343ca5a42e1aa9
Pavel Gajdoš <info@pavelgajdos.cz>
2015-03-23 21:33:15 +0100

    leanmapper filter

---
+++ src/LeanMapper/Filter.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace PG\LeanMapper\Filters;
+
+use LeanMapper\Connection;
+use LeanMapper\Fluent;
+use LeanMapper\IMapper;
+use LeanMapper\Reflection\Property;
+use Joseki\LeanMapper\BaseEntity;
+
+class Filter
+{
+    /** @var IMapper */
+    protected $mapper;
+
+    public function __construct(IMapper $mapper)
+    {
+        $this->mapper = $mapper;
+    }
+
+    public static function register(Filter $instance, Connection $connection)
+    {
+        $connection->registerFilter("orderBy", array($instance, "orderBy"),"ep");
+        $connection->registerFilter("orderByDesc", array($instance, "orderByDesc"),"ep");
+    }
+
+    public function orderBy(Fluent $statement, BaseEntity $entity, Property $property)
+    {
+        $orderByProperty = @func_get_arg(3);
+        $targetEntity = $this->mapper->getEntityClass($property->getRelationship()->getTargetTable());
+        $reflection = $targetEntity::getReflection();
+        $orderByColumn = $reflection->getEntityProperty($orderByProperty)->getColumn();
+        $statement->orderBy($orderByColumn);
+    }
+
+    public function orderByDesc(Fluent $statement, BaseEntity $entity, Property $property)
+    {
+        $orderByProperty = @func_get_arg(3);
+        $targetEntity = $this->mapper->getEntityClass($property->getRelationship()->getTargetTable());
+        $reflection = $targetEntity::getReflection();
+        $orderByColumn = $reflection->getEntityProperty($orderByProperty)->getColumn();
+        $statement->orderBy($orderByColumn." DESC");
+    }
+}
